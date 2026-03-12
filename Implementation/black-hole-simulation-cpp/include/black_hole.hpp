a98ea9b766aec2aa09fff4f99af65713dc62a2b5
MrGreenScout <davidlindkvist05@gmail.com>
2026-03-13 00:52:01 +0100

    Update black_hole.hpp

---
+++ Implementation/black-hole-simulation-cpp/include/black_hole.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+/** Represent a black hole with a mass and a schwartzchild radius */
+class BlackHole
+{
+public:
+    /** Constants */
+    constexpr static const double G_SI   = 6.674e-11;
+    constexpr static const double C_SI   = 3.0e8;
+
+    /** The mass of the black hole */
+    double mass;
+
+    /** The Schwartzchild radius */
+    double rs;
+
+    /** Geometric Mass unitless constant */
+    long double massGeo = G_SI * mass / (C_SI * C_SI);
+
+    BlackHole(double mass) : mass(mass) { rs = 2; }
+};
