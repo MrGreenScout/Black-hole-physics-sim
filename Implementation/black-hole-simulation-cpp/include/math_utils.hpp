c8997dc66b6dc1eecb868423733cfeb38c3243ae
MrGreenScout <davidlindkvist05@gmail.com>
2026-03-13 00:48:32 +0100

    Clear out old code

--- Implementation/black-hole-simulation-cpp/include/math_utils.hpp
+++ Implementation/black-hole-simulation-cpp/include/math_utils.hpp
@@ -10,28 +10,3 @@
 template <typename T> int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
-
-
-double bisection(double a, double b, std::function<double (double)> func)
-{   
-    double c = a;
-    for (size_t i = 0; i < 50; i++)
-    {
-        if (abs(b-a) < 1e-8) break;
-
-        double c = (b-a) / 2;
-
-        if (abs(func(c)) < 1e-8) break;
-
-        if (func(c) * func(a) < 0)
-        {
-            b = c;
-        }
-        else
-        {
-            a = c;
-        }
-    }
-    
-    return c;
-}
